10156b5b709897e541db62a2cfc4efddd34e1e47
edshPC <ed_shh@mail.ru>
2024-09-27 22:04:29 +0300

    Init

---
+++ src/test/java/com/edsh/is_lab1/IsLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edsh.is_lab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsLab1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
